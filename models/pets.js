32acfbda8d1c185cdc45b272e961839992e0dd7e
sue2021 <carpentersuzanne@johndeere.com>
2021-08-22 14:47:42 -0500

    Friday nite work commit

---
+++ models/pets.js
@@ -0,0 +1,33 @@
+const pets = [ 
+    { 
+        name: 'Yancy',
+        breed: 'Bulldog',
+        age: 10,
+        tricks: 'sit'
+    },
+    { 
+        name: 'Lucy',
+        breed: 'Poodle',
+        age: 10,
+        tricks: 'shake'
+    }
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = pets;
