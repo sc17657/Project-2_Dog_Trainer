5eaedad2e6c4b24c7d0f44fd1b40d0df08912c86
sue2021 <carpentersuzanne@johndeere.com>
2021-08-22 22:54:41 -0500

    Sunday night controllers commit

--- server.js
+++ server.js
@@ -1,90 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
 const methodOverride = require('method-override');
 
-const app = express();
-const pets = require('./models/pets');
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(methodOverride('_method'));
-
-
-
-app.get('/pets', (req, res) => {
-    res.render('index.ejs', { 
-    pets: pets
-    });
-})
-
-app.get('/pets/new', (req, res)=> {
-    res.render('new.ejs');
-});
-
-app.post('/pets', (req, res) => {
-    pets.push(req.body);
-    res.redirect('/pets/');
-});
-
-app.get('/pets/:index', function(req, res)  {
-    res.render('show.ejs', { 
-    pets: pets[req.params.index]
-    });
-
-});
-
-app.delete('/pets/:index', (req, res) => {
-	pets.splice(req.params.index, 1); 
-	res.redirect('/pets'); 
-});
-
-app.get('/pets/:index/edit', (req, res) => {
-	res.render('edit.ejs', { 
-		    pet: pets[req.params.index], 
-			index: req.params.index 
-        
-    })
-})
-
-app.put('/pets/:index', (req, res) => { 
-    pets[req.params.index] = req.body; 
-    console.log(pets)
-    res.redirect('/pets');
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/pets", require("./controllers/petsController.js"));
 
 
 app.listen(3000, ()=>{
