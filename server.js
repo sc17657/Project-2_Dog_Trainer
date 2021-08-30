bc90f0e6157ce93672f158189f1927c3af8ac0dc
sue2021 <carpentersuzanne@johndeere.com>
2021-08-30 16:12:28 -0500

    change to deploy app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
@@ -8,6 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/pets", require("./controllers/petsController.js"));
 app.use("/petParents", require("./controllers/petParentsController.js"));
 
-app.listen(3000, ()=>{
+app.listen(process.env.port, () => { 
     console.log("I am listening");
 });
